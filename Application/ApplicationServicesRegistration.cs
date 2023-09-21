8d0d92c8370b69644c683723680bcec14cc50a34
Szxro <szxro@gmail.com>
2023-09-20 22:13:44 -0400

    Registering mediatr, pipelines and validators

--- Application/ApplicationServicesRegistration.cs
+++ Application/ApplicationServicesRegistration.cs
@@ -1,7 +1,27 @@
-﻿namespace Application
-{
-    public class ApplicationServicesRegistration
-    {
+﻿using Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
+namespace Application;
+
+public static class ApplicationServicesRegistration
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        //Registering the mediatr and pipeline behaviors => Trasient
+        services.AddMediatR(options =>
+        {
+            options.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            options.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>)); // <,> => mean that all that enter is allow
+            options.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            options.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
+        });
+
+        //Registering the validations => Transient
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        return services;
     }
 }
