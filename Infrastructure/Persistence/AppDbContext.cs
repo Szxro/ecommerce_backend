bdb5d353cae1dc16e3410792ea88b02b88fc6af7
Szxro <szxro@gmail.com>
2023-09-20 21:30:02 -0400

    Add database context

---
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infrastructure.Persistence;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //Loading the configuration of the entities for Assembly
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<User> User => Set<User>();
+
+    public DbSet<UserHash> UserHash => Set<UserHash>();
+
+    public DbSet<UserSalt> UserSalt => Set<UserSalt>();
+
+    public DbSet<ShippingInfo> ShippingInfo => Set<ShippingInfo>(); 
+
+    public DbSet<Avatar> Avatar => Set<Avatar>();
+
+    public DbSet<Role> Role => Set<Role>();
+
+    public DbSet<UserRoles> UserRoles => Set<UserRoles>();
+
+    public DbSet<Product> Product => Set<Product>();
+
+    public DbSet<Categories> Categories => Set<Categories>();
+
+    public DbSet<ProductCategories> ProductCategories => Set<ProductCategories>();
+
+    public DbSet<ProductFiles> ProductFiles => Set<ProductFiles>(); 
+
+    public DbSet<Order> Order => Set<Order>();
+}
