fafc83093f4090a8e307cb9d52700fd217ae7fa8
Szxro <szxro@gmail.com>
2023-09-20 21:28:16 -0400

    Rename from ProductCategory to ProductCategories

---
+++ Domain/ProductCategories.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class ProductCategories : AuditableEntity
+{
+    public int? ProductId { get; set; }
+
+    public Product? Product { get; set; }
+
+    public int? CategoryId { get; set; }
+
+    public Categories? Category { get; set; }
+}
