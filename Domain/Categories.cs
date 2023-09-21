635048b5365970f4f6b529c6d9c82f05fcb09f06
Szxro <szxro@gmail.com>
2023-09-20 21:27:31 -0400

    Rename from Category to Categories

---
+++ Domain/Categories.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class Categories : AuditableEntity
+{
+    public Categories()
+    {
+        ProductCategories = new HashSet<ProductCategories>();    
+    }
+
+    public string CategoryName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<ProductCategories> ProductCategories { get; set; }
+}
