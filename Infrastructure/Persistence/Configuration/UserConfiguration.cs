9e82e7d34d6f69f2216ca0c4b9ffb4f201053ea7
Szxro <szxro@gmail.com>
2023-09-20 21:30:27 -0400

    Add configuration folder

---
+++ Infrastructure/Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configuration;
+
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasIndex(x => x.Username).IsUnique();
+
+        builder.HasIndex(x => x.Email).IsUnique();
+    }
+}
