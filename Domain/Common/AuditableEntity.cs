e6410ede0349614405b746b37aae6c1d84b9bc72
Szxro <szxro@gmail.com>
2023-09-20 21:35:27 -0400

    Making nullable CreatedAt & ModifiedAt

--- Domain/Common/AuditableEntity.cs
+++ Domain/Common/AuditableEntity.cs
@@ -4,7 +4,7 @@ public abstract class AuditableEntity
 {
     public int Id { get; set; }
 
-    public DateTime CreatedAt { get; set; }
+    public DateTime? CreatedAt { get; set; }
 
-    public DateTime ModifiedAt { get; set; }
+    public DateTime? ModifiedAt { get; set; }
 }
