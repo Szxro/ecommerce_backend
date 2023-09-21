50238f59a5fdb30f09e3f5316a46da7d9dc3fda2
Szxro <szxro@gmail.com>
2023-09-20 21:42:34 -0400

    Add ProductFiles & Avatar Configuration

---
+++ Infrastructure/Persistence/Configuration/AvatarConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configuration;
+
+internal class AvatarConfiguration : IEntityTypeConfiguration<Avatar>
+{
+    public void Configure(EntityTypeBuilder<Avatar> builder)
+    {
+        builder.OwnsOne(x => x.File, options =>
+        {
+            // To not have table_name_property_name (Avatar_FileName)
+            options.Property(x => x.FileName).HasColumnName("FileName");
+            options.Property(x => x.FileGuid).HasColumnName("FileGuid");
+            options.Property(x => x.FileExtension).HasColumnName("FileExtension");
+            options.Property(x => x.FilePath).HasColumnName("FilePath");
+        });
+    }
+}
---
+++ Infrastructure/Persistence/Configuration/ProductFilesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configuration;
+
+internal class ProductFilesConfiguration : IEntityTypeConfiguration<ProductFiles>
+{
+    public void Configure(EntityTypeBuilder<ProductFiles> builder)
+    {
+        builder.OwnsOne(x => x.File, options => 
+        {
+            options.Property(x => x.FileName).HasColumnName("FileName");
+            options.Property(x => x.FileGuid).HasColumnName("FileGuid");
+            options.Property(x => x.FileExtension).HasColumnName("FileExtension");
+            options.Property(x => x.FilePath).HasColumnName("FilePath");
+        });
+    }
+}
