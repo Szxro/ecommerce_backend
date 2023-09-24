c110028080eb1ba0c9a36c9af6cfbe0094a000bc
Szxro <szxro@gmail.com>
2023-09-23 21:45:59 -0400

    Add role repository class and functionality

---
+++ Infrastructure/Repositories/RoleRepository.cs
@@ -0,0 +1,19 @@
+﻿using Application.Common.Interfaces;
+using Domain;
+using Infrastructure.Common;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class RoleRepository : GenericRepository<Role>, IRoleRepository
+{
+    public RoleRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<Role?> GetRoleByRoleName(string roleName)
+    {
+        return await _context.Role.FirstOrDefaultAsync(role => role.RoleName == roleName);
+    }
+}
