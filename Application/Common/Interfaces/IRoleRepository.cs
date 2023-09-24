44c94f8bd2b3f80bddfaab005ec98af94719cdac
Szxro <szxro@gmail.com>
2023-09-23 22:28:49 -0400

    Add change tracker method

--- Application/Common/Interfaces/IRoleRepository.cs
+++ Application/Common/Interfaces/IRoleRepository.cs
@@ -5,4 +5,8 @@ namespace Application.Common.Interfaces;
 public interface IRoleRepository
 {
     Task<Role?> GetRoleByRoleName(string roleName);
+
+    void Add(Role newRole);
+
+    void ChangeTrackerToUnchanged(Role currentRole);
 }
--- Application/Common/Interfaces/IUserRepository.cs
+++ Application/Common/Interfaces/IUserRepository.cs
@@ -6,5 +6,9 @@ public interface IUserRepository
 {
     Task<User?> GetUserById(int id);
 
+    Task<User?> GetUserByUsername(string username);
+
     void Add(User newUser);
+
+    void ChangeTrackerToUnchanged(User user);
 }
