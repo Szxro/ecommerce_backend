017679cf58808eb6169bc7165f2dbd5f9282a3d7
Szxro <szxro@gmail.com>
2023-09-23 22:01:50 -0400

    Add user controller and create user endpoint

---
+++ Web Api/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Users.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("user/newuser")]
+
+        public async Task<ActionResult<Unit>> CreateNewUser(CreateUserCommand newUser)
+        {
+            return Ok(await _mediator.Send(newUser));
+        }
+    }
+}
