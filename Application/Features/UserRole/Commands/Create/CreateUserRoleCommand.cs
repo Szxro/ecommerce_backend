06841654b72f6ddaa1dc8b6dc6ad8fb066901558
Szxro <szxro@gmail.com>
2023-09-23 22:47:20 -0400

    Add user role command and validator

---
+++ Application/Features/UserRole/Commands/Create/CreateUserRoleCommand.cs
@@ -0,0 +1,55 @@
+﻿using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Domain;
+using MediatR;
+
+namespace Application.Features.UserRole.Commands.Create;
+
+public record CreateUserRoleCommand(string username,string rolename) : IRequest<Unit>;
+
+public class CreateUserRoleCommandHandler : IRequestHandler<CreateUserRoleCommand, Unit>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IUserRoleRepository _userRoleRepository;
+    private readonly IRoleRepository _roleRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateUserRoleCommandHandler(IUserRepository userRepository,
+                                        IUserRoleRepository userRoleRepository,
+                                        IRoleRepository roleRepository,
+                                        IUnitOfWork unitOfWork)
+    {
+        _userRepository = userRepository;
+        _userRoleRepository = userRoleRepository;
+        _roleRepository = roleRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(CreateUserRoleCommand request, CancellationToken cancellationToken)
+    {
+        User? currentUser = await _userRepository.GetUserByUsername(request.username);
+
+        if (currentUser is null) throw new NotFoundException($"The username <{request.username}> was not found");
+
+        Role? currentRole = await _roleRepository.GetRoleByRoleName(request.rolename);
+
+        if (currentRole is null) throw new NotFoundException($"The role with the rolename <{request.rolename}> was not found");
+
+        // Changing tracking to unchanged
+
+        _userRepository.ChangeTrackerToUnchanged(currentUser);
+        _roleRepository.ChangeTrackerToUnchanged(currentRole);
+
+        UserRoles newUserRoles = new()
+        {
+            UserId = currentUser.Id,
+            RoleId = currentRole.Id
+        };
+
+        _userRoleRepository.Add(newUserRoles);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
---
+++ Application/Features/UserRole/Commands/Create/CreateUserRoleCommandValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace Application.Features.UserRole.Commands.Create;
+
+public class CreateUserRoleCommandValidator : AbstractValidator<CreateUserRoleCommand>
+{
+    public CreateUserRoleCommandValidator()
+    {
+
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(userRole => userRole.username).NotEmpty().NotNull().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(userRole => userRole.rolename).EmailAddress().WithMessage("The {PropertyName} need to be a valid email");
+    }
+}
