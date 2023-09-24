6ee6b97aab8d35e77fae9ecd860bb745de1fc210
Szxro <szxro@gmail.com>
2023-09-23 21:55:48 -0400

    Add mapping class and manual mapping methods

---
+++ Application/Common/Mapping/Mapper.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Users.Commands.Create;
+using Domain;
+
+namespace Application.Common.Mapping;
+
+public static class Mapper
+{
+    public static User ToUser(this CreateUserCommand newUser,string userHash, byte[] userSalt)
+    {
+        return new User()
+        {
+            Username = newUser.Username,
+            Email = newUser.Email,
+            FullName = newUser.Fullname,
+            UserHash = new UserHash() {HashValue = userHash },
+            UserSalt = new UserSalt() { SaltValue = Convert.ToHexString(userSalt) }
+        };
+    }
+}
