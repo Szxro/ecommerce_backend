60736f148c5679bb6a1fce6f37de3050b0a64862
Szxro <szxro@gmail.com>
2023-09-23 22:00:38 -0400

    Add create user command

---
+++ Application/Features/Users/Commands/Create/CreateUserCommand.cs
@@ -0,0 +1,56 @@
+﻿using MediatR;
+using Domain;
+using Application.Common.Exceptions;
+using Application.Common.Interfaces;
+using Application.Common.Mapping;
+
+namespace Application.Features.Users.Commands.Create;
+
+public record CreateUserCommand : IRequest<Unit>
+{
+    public string Fullname { get; set; } = string.Empty;
+
+    public string Username { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+    public string ConfirmPassword { get; set; } = string.Empty;
+}
+
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IPasswordService _passwordService;
+    private readonly IUserRepository _user;
+
+    public CreateUserCommandHandler(
+        IUnitOfWork unitOfWork,
+        IPasswordService passwordService,
+        IUserRepository user,
+        IRoleRepository role)
+    {
+        _unitOfWork = unitOfWork;
+        _passwordService = passwordService;
+        _user = user;
+    }
+    public async Task<Unit> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+    {
+        if (!CheckPasswordEquality(request.Password, request.ConfirmPassword)) throw new PasswordException("The password and confirm password must be equal");
+
+        //Generating UserHash and UserSalt
+        string userHash = _passwordService.GenerateUserHashAndSalt(request.Password,out byte[] userSalt);
+
+        //Mapping from CreateUserCommand to User (Manual) 
+        User newUser = request.ToUser(userHash,userSalt);
+
+        _user.Add(newUser);
+
+        await _unitOfWork.SaveChangesAsync();
+
+        return Unit.Value;
+    }
+
+    private bool CheckPasswordEquality(string password,string confirmPassword) => password.Equals(confirmPassword);
+}
