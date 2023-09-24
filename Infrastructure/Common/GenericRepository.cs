510ecff8b2b04b6062528fa99ffd6ecb81c7a729
Szxro <szxro@gmail.com>
2023-09-23 21:01:02 -0400

    Add generic repository implementation

---
+++ Infrastructure/Common/GenericRepository.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Common;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Common;
+
+public abstract class GenericRepository<TEntity> where TEntity : AuditableEntity
+{
+    protected readonly AppDbContext _context;
+
+    public GenericRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public void Add(TEntity entity)
+    {
+        _context.Set<TEntity>().Add(entity);  
+    }
+
+    public void Delete(TEntity entity)
+    {
+        _context.Set<TEntity>().Remove(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+        _context.Set<TEntity>().Update(entity);
+    }
+}
