905b317171773ec50353a014ec00161dd2cd0b1e
Szxro <szxro@gmail.com>
2023-09-23 22:54:37 -0400

    Add user role controller

---
+++ Web Api/Controllers/UserRoleController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.UserRole.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRoleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserRoleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("userrole/addrole")]
+
+        public async Task<ActionResult<Unit>> AddUserRoleToUser(string username,string rolename = "User")
+        {
+            return await _mediator.Send(new CreateUserRoleCommand(username,rolename));
+        } 
+    }
+}
