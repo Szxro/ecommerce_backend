6fc31af2e8419965c5e7352565500b6210ab778f
Szxro <szxro@gmail.com>
2023-09-23 20:52:10 -0400

    Add password service interface

---
+++ Application/Common/Interfaces/IPasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common.Interfaces;
+
+public interface IPasswordService
+{
+    string GenerateUserHashAndSalt(string password,out byte[] salt);
+
+    bool CompareUserHash(string password, string hash, byte[] salt);
+}
