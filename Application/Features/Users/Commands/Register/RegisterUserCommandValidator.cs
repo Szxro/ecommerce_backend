9cb0ac4f10d8ec90ab2772aa404b1cf600dd6307
Szxro <szxro@gmail.com>
2023-10-01 19:04:32 -0400

    Rename from Create to Register to better understanding

---
+++ Application/Features/Users/Commands/Register/RegisterUserCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Users.Commands.Register;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        //In the first failure it stop and continue the next validation
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(user => user.Email).NotEmpty().NotNull().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(user => user.Email).EmailAddress().WithMessage("The {PropertyName} need to be a valid email");
+
+        RuleFor(user => user.Username).NotEmpty().NotNull().WithMessage("The {PropertyName} cant be empty");
+    }
+}
