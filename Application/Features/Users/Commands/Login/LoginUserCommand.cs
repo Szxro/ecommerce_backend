42fc35b55bb2bb5da970b49918b0bfdc912cb399
Szxro <szxro@gmail.com>
2023-10-01 19:00:36 -0400

    Add login command request and handler

---
+++ Application/Features/Users/Commands/Login/LoginUserCommand.cs
@@ -0,0 +1,49 @@
+﻿using MediatR;
+using Domain.Common.Response;
+using Application.Common.Interfaces;
+using Domain;
+using Application.Common.Exceptions;
+
+namespace Application.Features.Users.Commands.Login;
+
+public record LoginUserCommand(string username,string password) : IRequest<TokenResponse>;
+
+public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, TokenResponse>
+{
+    private readonly IUserRepository _user;
+    private readonly ITokenService _tokenService;
+    private readonly IPasswordService _password;
+
+    public LoginUserCommandHandler(IUserRepository user,
+                                   ITokenService tokenService,
+                                   IPasswordService password)
+    {
+        _user = user;
+        _tokenService = tokenService;
+        _password = password;
+    }
+    public async Task<TokenResponse> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+    {
+        User? currentUser = await _user.GetBy(user => user.Username == request.username, new List<string>() { "UserRoles", "UserHash", "UserSalt" });
+
+        if (currentUser is null) throw new NotFoundException($"The username <{request.username}> was not found");
+
+        string? currentUserHash = currentUser.UserHash?.HashValue;
+
+        if (string.IsNullOrWhiteSpace(currentUserHash)) throw new Exception("Invalid hash");
+
+        byte[]? currentUserSalt = Convert.FromHexString(currentUser.UserSalt!.SaltValue);
+
+        if (currentUserSalt is null) throw new Exception("Invalid salt");
+
+        bool isPasswordCorrect = _password.CompareUserHash(request.password,currentUserHash,currentUserSalt);
+
+        if (!isPasswordCorrect) throw new Exception("Incorrect password, try again");
+
+        //Generating the token and returning it as a response
+
+        string tokenResponse = await _tokenService.GenerateToken(currentUser);
+
+        return new TokenResponse(tokenResponse);
+    }
+}
---
+++ Application/Features/Users/Commands/Login/LoginUserCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Application.Features.Users.Commands.Login;
+
+public class LoginUserCommandValidator : AbstractValidator<LoginUserCommand>
+{
+    public LoginUserCommandValidator()
+    {
+        RuleLevelCascadeMode = CascadeMode.Stop;
+
+        RuleFor(prop => prop.username).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+
+        RuleFor(prop => prop.password).NotEmpty().WithMessage("The {PropertyName} cant be empty");
+    }
+}
