0b783baa8c0b29faddb244ba4632b40c6a00df62
Szxro <szxro@gmail.com>
2023-10-04 22:04:55 -0400

    Add and Use exception middleware

--- Web Api/Program.cs
+++ Web Api/Program.cs
@@ -1,15 +1,17 @@
 using Application;
 using Infrastructure;
 using Web_Api.Extensions;
+using Web_Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
 
     builder.Services.AddApplication();
-    builder.Services.AddInfrastructure(builder.Configuration);
+    builder.Services.AddInfrastructure();
     builder.Services.AddInfrastructureAuthentication(builder.Configuration);
     builder.Services.AddControllers();
+    builder.Services.AddExceptionMiddleware(); // Registering the middleware (have to register because it use the IMiddleware interface)
     // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.ConfigureSwagger();
@@ -27,6 +29,8 @@ var app = builder.Build();
 
         await app.InitializeDatabaseAsync(); // Migrating and Seeding if its necessary
     }
+
+    app.UseMiddleware<ExceptionMiddleware>(); // Using the middleware
 
     app.UseHttpsRedirection();
 
