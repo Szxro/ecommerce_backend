16664e736a6cbeaec765805fe07e8d8ec366ca77
Szxro <szxro@gmail.com>
2023-10-11 20:16:53 -0400

    Add method to validate and return some claims in tuple

--- Application/Common/Interfaces/ITokenService.cs
+++ Application/Common/Interfaces/ITokenService.cs
@@ -5,4 +5,7 @@ namespace Application.Common.Interfaces;
 public interface ITokenService
 {
     Task<string> GenerateToken(User currentUser, double tokenLifeTime = 10.00);
+
+    (List<string> Roles, string? tokenExpiryStamp) ValidateAndReturnClaims(string token);
+    //(type nameType,type nameType) => this is call a tuple
 }
