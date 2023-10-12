560b998f10f161f5a277ec0faa3224fc9bcbb3e3
Szxro <szxro@gmail.com>
2023-10-11 20:19:03 -0400

    Add Privilege table

---
+++ Domain/Privilege.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class Privilege : AuditableEntity
+{
+    public Privilege()
+    {
+        RolePrivileges = new HashSet<RolePrivilege>();
+    }
+
+    public string PrivilegeName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<RolePrivilege> RolePrivileges { get; set; }
+}
