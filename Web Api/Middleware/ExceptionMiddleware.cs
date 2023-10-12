3a80d8436d89a8ed90c72a107a17413c307cb46d
Szxro <szxro@gmail.com>
2023-10-11 20:53:25 -0400

    Improve indentation in the middleware

--- Web Api/Middleware/ExceptionMiddleware.cs
+++ Web Api/Middleware/ExceptionMiddleware.cs
@@ -3,68 +3,67 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net;
 using System.Text.Json;
 
-namespace Web_Api.Middleware
+namespace Web_Api.Middleware;
+
+public class ExceptionMiddleware : IMiddleware
 {
-    public class ExceptionMiddleware : IMiddleware
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        try
         {
-            try
-            {
-                await next.Invoke(context);
-            
-            } catch (Exception ex)
-            {
-                await HandleExceptionAsync(context, ex);
-            }
+            await next.Invoke(context);
+        
+        } catch (Exception ex)
+        {
+            await HandleExceptionAsync(context, ex);
+        }
+    }
+
+    private async Task HandleExceptionAsync(HttpContext context,Exception exception)
+    {
+        string result = string.Empty;
+
+        switch (exception)
+        {
+            case PasswordException passwordException:
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(new ProblemDetails()
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                    Title = $"{exception.GetType().Name} Happen",
+                    Detail = passwordException.Message
+                });
+                break;
+            case ValidationsException validationsException:
+                context.Response.StatusCode= (int)HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(new ValidationProblemDetails(validationsException._Errors)
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                    Title = "Validation Exception Happen"
+                });
+                break;
+            case NotFoundException notFoundException:
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                result = JsonSerializer.Serialize(new ProblemDetails()
+                {
+                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+                    Title = $"{exception.GetType().Name} Happen",
+                    Detail = notFoundException.Message
+                });
+                break;
+            default:
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                result = JsonSerializer.Serialize(new ProblemDetails()
+                {
+                    Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+                    Title = "Internal Server Error",
+                    Detail = exception.Message
+                });
+                break;
         }
 
-        private async Task HandleExceptionAsync(HttpContext context,Exception exception)
-        {
-            string result = string.Empty;
+        context.Response.ContentType = "application/json"; // important to write the content correctly
 
-            switch (exception)
-            {
-                case PasswordException passwordException:
-                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    result = JsonSerializer.Serialize(new ProblemDetails()
-                    {
-                        Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                        Title = $"{exception.GetType().Name} Happen",
-                        Detail = passwordException.Message
-                    });
-                    break;
-                case ValidationsException validationsException:
-                    context.Response.StatusCode= (int)HttpStatusCode.BadRequest;
-                    result = JsonSerializer.Serialize(new ValidationProblemDetails(validationsException._Errors)
-                    {
-                        Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                        Title = "Validation Exception Happen"
-                    });
-                    break;
-                case NotFoundException notFoundException:
-                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                    result = JsonSerializer.Serialize(new ProblemDetails()
-                    {
-                        Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                        Title = $"{exception.GetType().Name} Happen",
-                        Detail = notFoundException.Message
-                    });
-                    break;
-                default:
-                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                    result = JsonSerializer.Serialize(new ProblemDetails()
-                    {
-                        Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
-                        Title = "Internal Server Error",
-                        Detail = exception.Message
-                    });
-                    break;
-            }
-
-            context.Response.ContentType = "application/json"; // important to write the content correctly
-
-            await context.Response.WriteAsync(result);
-        }
+        await context.Response.WriteAsync(result);
     }
 }
