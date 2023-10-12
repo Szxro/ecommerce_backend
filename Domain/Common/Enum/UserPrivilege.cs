a3d99ffd7d2f4d935e7deef7460f87ec8ded95aa
Szxro <szxro@gmail.com>
2023-10-11 20:31:15 -0400

    Add enum of the current privilege of the roles

---
+++ Domain/Common/Enum/UserPrivilege.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Common.Enum;
+
+public enum UserPrivilege
+{
+    Write,
+    Read,
+    Delete,
+    Update,
+    WriteAdmin,
+    ReadAdmin,
+    DeleteAdmin,
+    UpdateAdmin,
+}
