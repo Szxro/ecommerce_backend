035d335d6788273492f46eb175957a72724c3cac
Szxro <szxro@gmail.com>
2023-10-11 20:18:27 -0400

    Add intermediary table between Privilege and Role table

---
+++ Domain/RolePrivilege.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class RolePrivilege : AuditableEntity
+{
+    public int? RoleId { get; set; }
+
+    public Role Role { get; set; } = new();
+
+    public int? PrivilegeId { get; set; }
+
+    public Privilege Privilige { get; set; } = new();
+}
