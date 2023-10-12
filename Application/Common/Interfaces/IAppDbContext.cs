79d5101f29c78607f27066f0ec2ec34968367d65
Szxro <szxro@gmail.com>
2023-10-11 20:22:50 -0400

    Add interface to make queries to specify tables

---
+++ Application/Common/Interfaces/IAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Common.Interfaces;
+
+public interface IAppDbContext
+{
+    DbSet<RolePrivilege> RolePrivilege { get;}
+
+    DbSet<Role> Role { get; }
+
+    DbSet<Privilege> Privileges { get; }
+}
