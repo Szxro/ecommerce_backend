34112b80462afb7eb851dd638fd4ff20a171c7e4
Szxro <szxro@gmail.com>
2023-10-11 20:30:27 -0400

    Add ValidateAndReturnClaims method in the class

--- Infrastructure/Services/TokenService.cs
+++ Infrastructure/Services/TokenService.cs
@@ -15,6 +15,7 @@ public class TokenService : ITokenService
     private readonly JwtOptions _jwtOptions;
     private readonly IDateService _date;
     private readonly IRoleRepository _role;
+    private readonly TokenValidationParameters _tokenValidationParameters;
 
     public TokenService(
         IOptions<JwtOptions> options,
@@ -24,6 +25,16 @@ public class TokenService : ITokenService
         _jwtOptions = options.Value;
         _date = date;
         _role = role;
+        _tokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = true,
+            ValidateIssuer = true,
+            ValidateLifetime = false, // to not have a time exception 
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = _jwtOptions.ValidIssuer,
+            ValidAudience = _jwtOptions.ValidAudience,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_jwtOptions.SecretKey))
+        };
     }
 
     public async Task<string> GenerateToken(User currentUser,double tokenLifeTime = 10.00) // by default is going 10 min to expire
@@ -52,11 +63,34 @@ public class TokenService : ITokenService
         return handler.WriteToken(token);
     }
 
+    public (List<string> Roles, string? tokenExpiryStamp) ValidateAndReturnClaims(string token)
+    {
+        JwtSecurityTokenHandler handler = new();
+
+        ClaimsPrincipal tokenClaims = handler.ValidateToken(token, _tokenValidationParameters, out SecurityToken validatedToken);
+
+        if (validatedToken is JwtSecurityToken jwtSecurityToken)
+        {
+            bool isTokenAlgorithmValid = jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha512, StringComparison.InvariantCultureIgnoreCase);
+
+            if (!isTokenAlgorithmValid) throw new Exception("Invalid Token Algorithm");
+        }
+
+        List<string> roles = tokenClaims.Claims
+                                        .Where(claim => claim.Type == ClaimTypes.Role)
+                                        .Select(claim => claim.Value)
+                                        .ToList();
+
+        string? tokenExpiryStamp = tokenClaims.Claims.FirstOrDefault(claim => claim.Type == JwtRegisteredClaimNames.Exp)?.Value;
+
+        return (roles, tokenExpiryStamp);
+    }
+
     private async Task<ClaimsIdentity> GenerateClaims(User user) 
     {
         List<int?> rolesId= user.UserRoles.Select(userRole => userRole.RoleId).ToList(); // returning the ids associated with the user
 
-        ICollection<Role> userRoles = await _role.GetUserRoles(rolesId); // returning the role associated with that id
+        ICollection<string> userRoles = await _role.GetUserRoleNames(rolesId); // returning the rolenames associated wih the id
 
         if (!userRoles.Any())
         {
@@ -69,7 +103,7 @@ public class TokenService : ITokenService
                 new Claim(JwtRegisteredClaimNames.Name,user.Username),
                 new Claim(JwtRegisteredClaimNames.Email, user.Email),
                 new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()) // Unique Guid by Users
-            }.Union(userRoles.Select(role => new Claim(ClaimTypes.Role, role.RoleName))).ToList());
+            }.Union(userRoles.Select(roleNames => new Claim(ClaimTypes.Role,roleNames))).ToList());
 
         return userClaims;
     }
