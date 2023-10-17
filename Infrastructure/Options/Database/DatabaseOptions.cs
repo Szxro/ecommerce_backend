7973c9bc099fb7db70d73deed0a3c5037c2cc171
Szxro <szxro@gmail.com>
2023-10-16 20:30:13 -0400

    Add new options

--- Infrastructure/Options/Database/DatabaseOptions.cs
+++ Infrastructure/Options/Database/DatabaseOptions.cs
@@ -5,4 +5,10 @@ public class DatabaseOptions
     public string ConnectionString { get; set; } = string.Empty;
 
     public int MaxRetryCount { get; set; }
+
+    public int CommandTimeout { get; set; }
+
+    public bool EnableDetailedErrors { get; set; }
+
+    public bool EnableSensitiveDataLogging { get; set; } 
 }
