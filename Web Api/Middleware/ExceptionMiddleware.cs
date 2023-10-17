9fc46faa814a3e9ccf82928754de273f94fc494e
Szxro <szxro@gmail.com>
2023-10-16 20:48:38 -0400

    Improve middleware 2.0

--- Web Api/Middleware/ExceptionMiddleware.cs
+++ Web Api/Middleware/ExceptionMiddleware.cs
@@ -21,49 +21,47 @@ public class ExceptionMiddleware : IMiddleware
 
     private async Task HandleExceptionAsync(HttpContext context,Exception exception)
     {
-        string result = string.Empty;
+        string result = GetErrorMessage(exception);
 
-        switch (exception)
-        {
-            case PasswordException passwordException:
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                result = JsonSerializer.Serialize(new ProblemDetails()
-                {
-                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                    Title = $"{exception.GetType().Name} Happen",
-                    Detail = passwordException.Message
-                });
-                break;
-            case ValidationsException validationsException:
-                context.Response.StatusCode= (int)HttpStatusCode.BadRequest;
-                result = JsonSerializer.Serialize(new ValidationProblemDetails(validationsException._Errors)
-                {
-                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                    Title = "Validation Exception Happen"
-                });
-                break;
-            case NotFoundException notFoundException:
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                result = JsonSerializer.Serialize(new ProblemDetails()
-                {
-                    Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
-                    Title = $"{exception.GetType().Name} Happen",
-                    Detail = notFoundException.Message
-                });
-                break;
-            default:
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-                result = JsonSerializer.Serialize(new ProblemDetails()
-                {
-                    Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
-                    Title = "Internal Server Error",
-                    Detail = exception.Message
-                });
-                break;
-        }
+        context.Response.StatusCode = GetHttpStatusCode(exception);
 
         context.Response.ContentType = "application/json"; // important to write the content correctly
 
         await context.Response.WriteAsync(result);
     }
+
+    private int GetHttpStatusCode(Exception exception) => exception switch
+    {
+        PasswordException passwordException => (int)HttpStatusCode.BadRequest,
+        ValidationsException validationsException=> (int)HttpStatusCode.BadRequest,
+        NotFoundException notFoundException => (int)HttpStatusCode.NotFound,
+        _ => (int)HttpStatusCode.InternalServerError
+    };
+
+    private string GetErrorMessage(Exception exception) => exception switch
+    {
+        PasswordException passwordException => JsonSerializer.Serialize(new ProblemDetails()
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = $"{exception.GetType().Name} Happen",
+            Detail = passwordException.Message
+        }),
+        ValidationsException validationsException => JsonSerializer.Serialize(new ValidationProblemDetails(validationsException._Errors)
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = "Validation Exception Happen"
+        }),
+        NotFoundException notFoundException => JsonSerializer.Serialize(new ProblemDetails()
+        {
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.4",
+            Title = $"{exception.GetType().Name} Happen",
+            Detail = notFoundException.Message
+        }),
+        _ => JsonSerializer.Serialize(new ProblemDetails()
+        {
+            Type = "https://datatracker.ietf.org/doc/html/rfc7231#section-6.6.1",
+            Title = "Internal Server Error",
+            Detail = exception.Message
+        })
+    };
 }
