917d8ce28517de80600aecc65685d2716b1a5533
Szxro <szxro@gmail.com>
2023-11-01 21:35:10 -0400

    Modify privilege filter functionality

--- Web Api/Filters/PrivilegeFilter.cs
+++ Web Api/Filters/PrivilegeFilter.cs
@@ -4,28 +4,29 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
+using System.Security.Claims;
 
 namespace Web_Api.Filters;
 
 [AttributeUsage(AttributeTargets.Method)]
 public class PrivilegeFilter : Attribute, IAsyncAuthorizationFilter
 {
-    private readonly UserPrivilege _userPrivilege;
-    private readonly IDictionary<UserPrivilege, string> _rolePrivilege = new Dictionary<UserPrivilege, string>();
+    private readonly UserScope _userScope;
+    private readonly IDictionary<UserScope, string> _roleScope = new Dictionary<UserScope, string>();
 
-    public PrivilegeFilter(UserPrivilege userPrivilege)
+    public PrivilegeFilter(UserScope userScope)
     {
-        _userPrivilege = userPrivilege;
-        _rolePrivilege = new Dictionary<UserPrivilege, string>()
+        _userScope = userScope;
+        _roleScope = new Dictionary<UserScope, string>()
         {
-            {UserPrivilege.Write,"Write"},
-            {UserPrivilege.Read,"Read"},
-            {UserPrivilege.Update,"Update"},
-            {UserPrivilege.Delete,"Delete"},
-            {UserPrivilege.WriteAdmin,"WriteAdmin"},
-            {UserPrivilege.ReadAdmin,"ReadAdmin"},
-            {UserPrivilege.UpdateAdmin,"UpdateAdmin"},
-            {UserPrivilege.DeleteAdmin,"DeleteAdmin"}
+            {UserScope.Write,"Write"},
+            {UserScope.Read,"Read"},
+            {UserScope.Update,"Update"},
+            {UserScope.Delete,"Delete"},
+            {UserScope.WriteAdmin,"WriteAdmin"},
+            {UserScope.ReadAdmin,"ReadAdmin"},
+            {UserScope.UpdateAdmin,"UpdateAdmin"},
+            {UserScope.DeleteAdmin,"DeleteAdmin"}
         };
     }
 
@@ -38,36 +39,35 @@ public class PrivilegeFilter : Attribute, IAsyncAuthorizationFilter
 
         IAppDbContext? dbContext = context.HttpContext.RequestServices.GetRequiredService<IAppDbContext>();
 
-        // Getting the roles that was place in the httpContext
-        object? roles = context.HttpContext.Items["roles"];
+        // Getting the roles of the current user
+        List<string>? currentUserRoles = context.HttpContext.User?.Claims
+                                                .Where(claim => claim.Type == ClaimTypes.Role)
+                                                .Select(x => x.Value)
+                                                .ToList();
 
-        if (roles is null) throw new UnauthorizedAccessException();
+        if (currentUserRoles is null) throw new UnauthorizedAccessException();
 
-        if (!await ValidateRolePrivilige(roles, dbContext)) throw new UnauthorizedAccessException();
+        if (!await ValidateRolePrivilige(currentUserRoles, dbContext)) throw new UnauthorizedAccessException();
     }
 
-    private async Task<bool> ValidateRolePrivilige(object? roles, IAppDbContext? dbContext)
+    private async Task<bool> ValidateRolePrivilige(List<string>? roles, IAppDbContext? dbContext)
     {
         if (dbContext is null || roles is null) return false;
 
-        if (roles is List<string> roleNames)
-        {
-            //Returning the count of the existing roles base on the rolenames 
-            int rolesCount = await dbContext.Role.Where(role => roleNames.Contains(role.RoleName))
+        //Returning the count of the existing roles base on the rolenames 
+        int rolesCount = await dbContext.Role.Where(role => roles.Contains(role.RoleName))
                                              .Select(role => role.RoleName)
                                              .Distinct()
                                              .CountAsync();
                               
-            // if the rolesCount is diferent from the roleNames 
-            if (rolesCount != roleNames.Count) throw new UnauthorizedAccessException();
+        // if the rolesCount is diferent from the roleNames 
+        if (rolesCount != roles.Count) throw new UnauthorizedAccessException();
 
-            // Check if exists a combination with Rolenames pass and the necessary privilege to do the action
-            return await dbContext.RolePrivilege
-                                         .Include(rolePrivilege => rolePrivilege.Role)
-                                         .Include(rolePrivilege => rolePrivilege.Privilige)
-                                         .Where(rolePrivilege => roleNames.Contains(rolePrivilege.Role.RoleName))
-                                         .AnyAsync(rolePrivilege => rolePrivilege.Privilige.PrivilegeName == _rolePrivilege[_userPrivilege]);
-        }
-        return false;
+        // Check if exists a combination with Rolenames pass and the necessary privilege to do the action
+        return await dbContext.RolePrivilege
+                               .Include(rolePrivilege => rolePrivilege.Role)
+                               .Include(rolePrivilege => rolePrivilege.Privilige)
+                               .Where(rolePrivilege => roles.Contains(rolePrivilege.Role.RoleName))
+                               .AnyAsync(rolePrivilege => rolePrivilege.Privilige.PrivilegeName == _roleScope[_userScope]);
     }
 }
