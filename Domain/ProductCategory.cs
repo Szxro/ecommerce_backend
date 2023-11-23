4bfa060d76b9e77d04331b24e9bec463b9d44afc
Szxro <szxro@gmail.com>
2023-11-22 22:45:41 -0400

    Add missing domain model

---
+++ Domain/ProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class ProductCategory : AuditableEntity
+{
+    public int ProductId { get; set; }
+
+    public Product Product { get; set; } = new();
+
+    public int CategoryId { get; set; }
+
+    public Category Category { get; set; } = new();
+}
