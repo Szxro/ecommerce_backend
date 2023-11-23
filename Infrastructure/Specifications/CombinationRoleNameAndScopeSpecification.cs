9f9424910e02753d1c53b7fb152ccb20bce7cc74
Szxro <szxro@gmail.com>
2023-11-22 22:55:53 -0400

    Update and rename from CombinationRoleNameAndPrivilegeSpecification to CombinationRoleNameAndScopeSpecification

---
+++ Infrastructure/Specifications/CombinationRoleNameAndScopeSpecification.cs
@@ -0,0 +1,16 @@
+﻿using Domain;
+using Domain.Common.Enum;
+using Infrastructure.Common;
+
+namespace Infrastructure.Specifications;
+
+public class CombinationRoleNameAndScopeSpecification : Specification<RoleScope>
+{
+    public CombinationRoleNameAndScopeSpecification(List<string> roles,
+                                                        IDictionary<UserScope, string> rolesScope)
+        :base(roleScope => roles.Contains(roleScope.Role!.RoleName))
+    {
+        AddInclude(roleScope => roleScope.Scope!);
+        AddInclude(roleScope => roleScope.Role!);
+    }
+}
