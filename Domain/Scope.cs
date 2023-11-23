a03dd320ef8bdde614586a79ff37a9d925474f8c
Szxro <szxro@gmail.com>
2023-11-22 22:35:06 -0400

    Rename from Privilege to Scope

---
+++ Domain/Scope.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+
+namespace Domain;
+
+public class Scope : AuditableEntity
+{
+    public Scope()
+    {
+        RoleScope = new HashSet<RoleScope>();
+    }
+
+    public string ScopeName { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public ICollection<RoleScope> RoleScope { get; set; }
+}
