3190405f198d4585f3b83ca41b403e373963ed9b
Szxro <szxro@gmail.com>
2023-11-27 17:06:50 -0400

    Update models to have relationship between them

--- Domain/Provider.cs
+++ Domain/Provider.cs
@@ -9,7 +9,12 @@ public class Provider : AuditableEntity
         UserPaymentMethods = new HashSet<UserPaymentMethod>();    
     }
 
+
     public string ProviderName { get; set; } = string.Empty;
+
+    public int PaymentTypeId { get; set; }
+
+    public PaymentType PaymentType { get; set; } = new();
 
     public ICollection<UserPaymentMethod> UserPaymentMethods { get; set; }
 }
