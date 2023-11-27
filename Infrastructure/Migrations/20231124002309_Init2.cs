ced1a7f47b5ca58471a9e4d01e20ed415117d0a5
Szxro <szxro@gmail.com>
2023-11-27 17:09:43 -0400

    Add table field and new relationship between tables

---
+++ Infrastructure/Migrations/20231124002309_Init2.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentTypeId",
+                table: "Provider",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Provider_PaymentTypeId",
+                table: "Provider",
+                column: "PaymentTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Provider_PaymentType_PaymentTypeId",
+                table: "Provider",
+                column: "PaymentTypeId",
+                principalTable: "PaymentType",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Provider_PaymentType_PaymentTypeId",
+                table: "Provider");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Provider_PaymentTypeId",
+                table: "Provider");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentTypeId",
+                table: "Provider");
+        }
+    }
+}
