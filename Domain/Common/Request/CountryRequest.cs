c5ba3673abac57b08b445bf301e7541157621bcb
Szxro <szxro@gmail.com>
2023-11-27 16:59:24 -0400

    Add model of the result of the api call

---
+++ Domain/Common/Request/CountryRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Domain.Common.Request;
+
+public partial class CountryRequest
+{
+    [JsonPropertyName("name")]
+    public Name Name { get; set; } = new();
+}
+
+public partial class Name
+{
+    [JsonPropertyName("common")]
+    public string Common { get; set; } = string.Empty;
+
+    [JsonPropertyName("official")]
+    public string Official { get; set; } = string.Empty;
+
+    [JsonPropertyName("nativeName")]
+    public Dictionary<string, NativeName> NativeName { get; set; } = new();
+}
+
+public partial class NativeName
+{
+    [JsonPropertyName("official")]
+    public string Official { get; set; } = string.Empty;
+
+    [JsonPropertyName("common")]
+    public string Common { get; set; } = string.Empty;
+}
