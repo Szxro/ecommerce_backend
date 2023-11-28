a6954236c9b35a4b4890074ad854d60a04052e90
Szxro <szxro@gmail.com>
2023-11-28 12:41:43 -0400

    Add guards utility

---
+++ Domain/Guards/Ensure.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Guards;
+
+// An entry point to a set of Guard Clauses defined as extension methods on IGuardClause.
+public interface IEnsure { }
+
+
+public class Ensure : IEnsure
+{
+    // only way to create a guard class
+    public static IEnsure Against = new Ensure();
+
+    private Ensure() { }
+}
---
+++ Domain/Guards/Extensions/EnsureAgainstNotNull.cs
@@ -0,0 +1,72 @@
+﻿using Domain.Exceptions;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace Domain.Guards.Extensions;
+
+public static partial class EnsureAgainstNotNull
+{
+    public static T NotNull<T>(
+       this IEnsure ensure,
+       [NotNull] T? input, // specifies that the output is not null
+       [CallerArgumentExpression("input")] string? paramName = null,
+       string? message = null)
+    {
+        if (input is null)
+        {
+            throw new NullException(message ?? $"Required input {paramName} was empty");
+        }
+
+        return input;
+    }
+
+    public static string NotNullOrEmpty(
+        this IEnsure ensure,
+        [NotNull] string? input,
+        [CallerArgumentExpression("input")] string? paramName = null,
+        string? message = null)
+    {
+        ensure.NotNull(input, paramName, message);
+
+        if (input == string.Empty)
+        {
+            throw new NullException(message ?? $"Required input {paramName} was empty");
+        }
+
+        return input;
+    }
+
+    public static IEnumerable<T> NotNullOrEmpty<T>(
+        this IEnsure ensure,
+        [NotNull] IEnumerable<T>? input,
+        [CallerArgumentExpression("input")] string? paramName = null,
+        string? message = null
+        )
+    {
+        ensure.NotNull(input, paramName, message);
+
+        if (!input.Any())
+        {
+            throw new NullException(message ?? $"Required collection {paramName} was empty");
+        }
+
+        return input;
+    }
+
+
+    public static string NotNullOrWhiteSpace(
+        this IEnsure ensure,
+        [NotNull] string? input,
+        [CallerArgumentExpression("input")] string? paramName = null,
+        string? message = null)
+    {
+        ensure.NotNullOrEmpty(input, paramName, message);
+
+        if (string.IsNullOrWhiteSpace(input))
+        {
+            throw new NullException(message ?? $"Required input {paramName} was empty");
+        }
+
+        return input;
+    }
+}
