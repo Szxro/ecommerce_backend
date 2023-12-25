033d94ad55019caad9e8e006e419ba06da0d7a49
Szxro <szxro@gmail.com>
2023-12-25 16:31:39 -0400

    Add interface for the UserActivityRepository

---
+++ Application/Common/Interfaces/IUserActivityRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+
+namespace Application.Common.Interfaces;
+
+public interface IUserActivityRepository
+{
+    Task<bool> IsUserAlreadyRegisterByUsernameAsync(string username);
+
+    Task InsertUserActivity(User user, DateTime loggedIn, CancellationToken cancellationToken = default);
+
+    Task<int?> UpdateUserActivityOfflinePropertyByUsername(string username, bool userState);
+
+    Task<int?> UpdateUserLoggedDateInByUsername(string username, DateTime dateTime);
+}
