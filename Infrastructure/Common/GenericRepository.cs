3b6f045f2bb5e218cb020baf8bd7bb2aabb464df
Szxro <szxro@gmail.com>
2023-12-25 16:20:55 -0400

    Modified the generic repository class with some improvements

--- Infrastructure/Common/GenericRepository.cs
+++ Infrastructure/Common/GenericRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Domain.Common;
 using Infrastructure.Persistence;
+using Infrastructure.Specifications;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
@@ -30,28 +31,52 @@ public abstract class GenericRepository<TEntity> where TEntity : AuditableEntity
         _context.Set<TEntity>().AddRange(entities);
     }
 
-    public Task DeleteByAsync(Expression<Func<TEntity,bool>> expression)
+    public async Task<int?> DeleteByAsync(Expression<Func<TEntity,bool>> expression)
     {
         // return how many rows where deleted
-        return _context.Set<TEntity>().Where(expression).ExecuteDeleteAsync();
+        return await _context.Set<TEntity>().Where(expression).ExecuteDeleteAsync();
     }
 
-    public Task UpdateByAsync(Expression<Func<TEntity, bool>> whereExpression,
-                              Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> propertiesExpression)
+    public async Task<int?> UpdateByAsync(Expression<Func<TEntity, bool>> whereExpression,
+                                          Expression<Func<SetPropertyCalls<TEntity>, SetPropertyCalls<TEntity>>> propertiesExpression,
+                                          List<string>? includeExpressions = null)
     {
-        // return how many rows were affected by the update
-        return _context.Set<TEntity>().Where(whereExpression).ExecuteUpdateAsync(propertiesExpression);
+        IQueryable<TEntity> queryable = _context.Set<TEntity>();
 
+        if (includeExpressions is not null)
+        {
+            includeExpressions.Aggregate(queryable, (current, includeExpression) => current.Include(includeExpression));
+        }
+
+        // return how many rows were affected by the update
+        return await queryable.Where(whereExpression).ExecuteUpdateAsync(propertiesExpression);
         // properties expression are those who specifies a property and its corresponding value (its used by the execute update method) 
     }
 
-    public void ChangeTrackerToUnchanged(TEntity entity)
+    public void ChangeEntityContextTracker(object entity,EntityState entityState)
     {
-        _context.Entry(entity).State = EntityState.Unchanged;
+        _context.Entry(entity).State = entityState;
     }
 
     public bool CheckHaveAnyData()
     {
         return _context.Set<TEntity>().Any();
+    }
+
+    public async Task<bool> CheckHaveAnyDataAsync()
+    {
+        return await _context.Set<TEntity>().AnyAsync();
+    }
+
+    public async Task<bool> CheckPropertyExistAsync(Expression<Func<TEntity,bool>> expression, List<string>? includeStatements = null)
+    {
+        IQueryable<TEntity> queryable = _context.Set<TEntity>();
+
+        if (includeStatements is not null)
+        {
+            includeStatements.Aggregate(queryable, (current, include) => current.Include(include));
+        }
+
+        return await queryable.AnyAsync(expression);
     }
 }
